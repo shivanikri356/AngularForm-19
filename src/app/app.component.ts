c27fdc31407def4b65580a7d1b2edbe9cda9da64
shivani <shivanikri356@gmail.com>
2024-11-20 18:43:01 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,12 @@ import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./reactive-form.component.css']
 })
 export class AppComponent implements OnInit {
   form: FormGroup;
   submittedData: any[] = [];
   subjectsFormArray: FormArray;
+data: any;
 
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
@@ -19,30 +19,40 @@ export class AppComponent implements OnInit {
       gender: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       mobile: ['', [Validators.required, Validators.pattern(/^[0-9]{10}$/)]],
-      dob: ['', Validators.required],  // Add dob control
-      subjects: this.fb.array([])
+      dob: ['', Validators.required],
+      subjects: this.fb.array([])  // Initialize the form array
     });
     this.subjectsFormArray = this.form.get('subjects') as FormArray;
   }
 
   ngOnInit(): void {}
 
+  // Add new subject to the FormArray
   addSubject() {
-    this.subjectsFormArray.push(this.fb.group({
-      subjectTitle: ['', Validators.required],
-      marks: ['', [Validators.required, Validators.min(0)]]
-    }));
+    const subjectGroup = this.fb.group({
+      subjectTitle: ['',],
+      marks: ['',]
+    });
+    this.subjectsFormArray.push(subjectGroup);
   }
 
+  // Remove subject from the FormArray
   removeSubject(index: number) {
     this.subjectsFormArray.removeAt(index);
   }
 
+  // Submit form data
   onSubmit() {
     if (this.form.valid) {
+     
       this.submittedData.push(this.form.value);
+      console.log('Form submitted:', this.submittedData);
+
       this.form.reset();
+     
       this.subjectsFormArray.clear();
+    } else {
+      console.log("Form is invalid");
     }
   }
 }
